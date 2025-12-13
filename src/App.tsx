353c5907b93099737abe77d28789e49aced7bd42
Mateusz <mateusz@matt-dev.pl>
2025-12-13 12:35:32 +0100

    add usage of MStatusFilter molecule

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import AButton from '@atoms/AButton/AButton';
-import MSection from '@molecules/MSection/MSection';
+import MColorFilter from '@molecules/MColorFilter/MColorFilter';
+import { AVAILABLE_COLORS } from '@utils/general';
+import { useState } from 'react';
 
 function App() {
+  const [selectedColors, setSelectedColors] = useState(['orange']);
+
+  const handleColorChange = (color: string, actionType: 'added' | 'removed') => {
+    const selectedColorsAfterChange =
+      actionType === 'removed'
+        ? selectedColors.filter((selectedColor) => selectedColor !== color)
+        : [...selectedColors, color];
+
+    setSelectedColors(selectedColorsAfterChange);
+  };
+
   return (
-    <MSection style={{ width: '500px' }} title="Przykładowa sekcja">
-      <AButton type="secondary">Przykładowy button</AButton>
-    </MSection>
+    <MColorFilter
+      availableColors={AVAILABLE_COLORS}
+      selectedColors={selectedColors}
+      onColorChange={handleColorChange}
+    />
   );
 }
 
